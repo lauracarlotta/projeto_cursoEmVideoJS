e764414795d25d3b33e81dc3afca99bc108a4fe5
Laura Carlotta <carlotta.custodio@gmail.com>
2020-08-28 20:47:35 -0300

    undestanding array

---
+++ js/aula15/ambiente.js
@@ -0,0 +1,10 @@
+// let num = []  --->  variável vazia
+ 
+let num = [3,2,5,4,6]
+
+
+num.sort()
+num.push(1)
+console.log(num)
+console.log(`O vetor tem ${num.length} posições!`)
+console.log(`O segundo valor do vetor é o número ${num[2]}`) // assim mostrará isoladamente
