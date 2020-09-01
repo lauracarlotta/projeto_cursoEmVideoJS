b62fb6fa8782dfe40d2f052390409f18a5b1e48c
Laura Carlotta <carlotta.custodio@gmail.com>
2020-09-01 15:53:47 -0300

    finishing function class

--- js/aula16/function03.js
+++ js/aula16/function03.js
@@ -1,5 +1,7 @@
 // Podemos ainda colocar uma function dentro de uma variável
 
-let x = function(){
-    
+let x = function(n){
+    return n*2
 }
+
+console.log(x(6)) // Ou seja, eu passei pra minha variável um parâmetro e ele conseguiu executar uma função!
